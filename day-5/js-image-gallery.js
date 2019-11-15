4617ad17a0cec8ef30d12dcbcfc48176e1e3e821
Dawit <dawit@ginetta.net>
2019-11-15 13:35:23 +0100

    Flex width and text animation, also with done with JS

---
+++ day-5/js-image-gallery.js
@@ -0,0 +1,18 @@
+// grab all the elements with the class panel
+const panels = document.querySelectorAll('.panel');
+
+function onToggleOpen() {
+	console.log('Hello');
+	this.classList.toggle('open');
+	console.log(this);
+}
+
+function toggleActive(event) {
+	// console.log(event.propertyName);
+	// this.classList.toggle(open-active);
+	if (event.propertyName === 'flex-grow') {
+		this.classList.toggle('open-active');
+	}
+}
+panels.forEach((panel) => panel.addEventListener('click', onToggleOpen));
+panels.forEach((panel) => panel.addEventListener('transitionend', toggleActive));
