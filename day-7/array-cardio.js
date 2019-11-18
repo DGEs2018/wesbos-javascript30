290a700c58e62049c254050e7686aa61f914ce52
Dawit <dawit@ginetta.net>
2019-11-18 20:00:41 +0100

    Add the continuation of array cardio exercise here

--- day-7/array-cardio.js
+++ day-7/array-cardio.js
@@ -141,3 +141,48 @@ const orderedByLastname = people.sort(function(last, next) {
 	// console.log(next);
 	// const nameSplitted = last.split(',');
 });
+
+const people = [
+	{ name: 'Wes', year: 1988 },
+	{ name: 'Kait', year: 1986 },
+	{ name: 'Irv', year: 1970 },
+	{ name: 'Lux', year: 2015 }
+];
+
+const comments = [
+	{ text: 'Love this!', id: 523423 },
+	{ text: 'Super good', id: 823423 },
+	{ text: 'You are the best', id: 2039842 },
+	{ text: 'Ramen is my fav food ever', id: 123523 },
+	{ text: 'Nice Nice Nice!', id: 542328 }
+];
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+const isAbove19 = people.some(function(person) {
+	// asssigns a variable called isAbove19, for those older than 19
+	const currentYear = new Date().getFullYear();
+	if (currentYear - person.year >= 19) {
+		return true;
+	}
+});
+
+console.log(isAbove19);
+// Array.prototype.every() // is everyone 19 or older?
+
+const isAbove19 = people.some((person) => {
+	// asssigns a variable called isAbove19, for those older than 19
+	// const currentYear = (new Date()).getFullYear();
+
+	return new Date().getFullYear() - person.year >= 19; // another way to put it, instead of having to define currentYear and put it inside a variable first ;
+
+	return currentYear - person.year >= 19;
+});
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
