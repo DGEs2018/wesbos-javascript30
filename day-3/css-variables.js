5c7a33d01502a14ed7549b3058f11f2a2234bcb8
Dawit <dawit@ginetta.net>
2019-11-20 07:31:59 +0100

    Add in the console logs to see what are some of the variables

--- day-3/css-variables.js
+++ day-3/css-variables.js
@@ -1,9 +1,14 @@
 const allInputs = document.querySelectorAll('.controls input');
 
 function handleUpdate() {
-	console.log(this.value);
-
-	const suffix = this.dataset;
+	// console.log(this.value);
+	const suffix = this.dataset.sizing || '';
+	document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+	console.log(this.name);
+	console.log(document.documentElement);
+	console.log(document);
+	console.log(this.dataset);
 }
 
 allInputs.forEach((input) => input.addEventListener('change', handleUpdate));
+allInputs.forEach((input) => input.addEventListener('mousemove', handleUpdate));
