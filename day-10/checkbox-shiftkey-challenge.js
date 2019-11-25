fe159ab0c300f7892b057c92bfa1683db7a78a07
Dawit <dawit@ginetta.net>
2019-11-25 09:33:46 +0100

    Write all the logic, set a variable inBetween to mark the top and bottom of the selected boxes, set it initially to false

--- day-10/checkbox-shiftkey-challenge.js
+++ day-10/checkbox-shiftkey-challenge.js
@@ -2,33 +2,63 @@ const checkBoxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
 let lastChecked;
 
-const handleCheck = (event) => {
-	console.log(event);
-	// check if the shiftKey is down
-	// and check if boxes are being checked
+const handleCheck = (e) => {
+	//check if they had the shift key pressed
 
+	// and check that the checkboxes are being checked
 	let inBetween = false;
+	if (e.shiftkey && this.checked) {
+		console.log(this);
+		// proceed
 
-	if (event.shiftKey && this.lastchecked) {
-		// then if these both are fulfilled
-
-		// loop through each box
+		// loop over each and every of the checkbox
 		checkBoxes.forEach((checkbox) => {
-			console.log(box);
-
+			console.log(checkbox);
 			if (checkbox === this || checkbox === lastChecked) {
-				// what's this line saying exactly ?
+				console.log(this);
 				inBetween = !inBetween;
-				console.log('checking them inbetween!');
+				console.log("Checking what's inbetween!");
 			}
 			if (inBetween) {
 				checkbox.checked = true;
 			}
 		});
 	}
+
 	lastChecked = this;
+	// console.log(e);
 };
 
+checkBoxes.forEach((checkbox) => checkbox.addEventListener('click', handleCheck));
+// let lastChecked;
+
+// const handleCheck = (event) => {
+// 	console.log(event);
+// 	// check if the shiftKey is down
+// 	// and check if boxes are being checked
+
+// 	let inBetween = false;
+
+// 	if (event.shiftKey && this.lastchecked) {
+// 		// then if these both are fulfilled
+
+// 		// loop through each box
+// 		checkBoxes.forEach((checkbox) => {
+// 			console.log(box);
+
+// 			if (checkbox === this || checkbox === lastChecked) {
+// 				// what's this line saying exactly ?
+// 				inBetween = !inBetween;
+// 				console.log('checking them inbetween!');
+// 			}
+// 			if (inBetween) {
+// 				checkbox.checked = true;
+// 			}
+// 		});
+// 	}
+// 	lastChecked = this;
+// };
+
 // checkBoxes.addEventListener('click', handleCheck);
-checkBoxes.forEach((box) => box.addEventListener('click', handleCheck));
+// checkBoxes.forEach((box) => box.addEventListener('click', handleCheck));
 // if (document)
