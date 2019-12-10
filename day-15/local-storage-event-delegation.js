4a910a9896bf9dd50940ff2239d922a4a1a51786
Dawit <dawit@ginetta.net>
2019-12-10 16:11:44 +0100

    Work the slight bit more left on local-storage and event delegation

--- day-15/local-storage-event-delegation.js
+++ day-15/local-storage-event-delegation.js
@@ -5,7 +5,8 @@ const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 
 //
-const items = JSON.parse(localStorage.getItem('items')) || [];
+const items = JSON.parse(localStorage.getItem('items')) || []; // when you put an item into the local storage, on page reload we check local storage
+// and fall back on an empty array is there's no object
 
 function addItem(e) {
 	e.preventDefault();
@@ -27,18 +28,27 @@ function addItem(e) {
 	localStorage.setItem('items', JSON.stringify(items));
 	this.reset();
 	console.log(this);
+}
 
-	function populateList(plates = [], platesList) {
-		platesList.innerHTML = plates
-			.map((plate, i) => {
-				return `
+function populateList(plates = [], platesList) {
+	platesList.innerHTML = plates
+		.map((plate, i) => {
+			return `
             <li>
                 <input type="checkbox" data-index=${i} id="item${i}" checked/>
                 <label for="">${plate.text}</label>
             </li>
             `;
-			})
-			.join(''); // map is going to return an array, with join we will make it then one big string
-	}
+		})
+		.join(''); // map is going to return an array, with join we will make it then one big string
 }
+function toggleDone() {}
+
 addItems.addEventListener('submit', addItem);
+
+const checkBoxes = document.querySelectorAll('.input');
+checkBoxes.forEach((input) => input.addEventListener('click', () => alert('checking the trigger')));
+
+populateList(items, itemsList);
+
+populateList;
